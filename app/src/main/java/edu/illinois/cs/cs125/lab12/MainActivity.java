698e4d287110f13e9a4da145587916accc14ee15
kevin_liu <kevindl2@illinois.edu>
2018-04-18 12:15:36 -0500

    Basic UI draft

--- app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
@@ -3,6 +3,10 @@ package edu.illinois.cs.cs125.lab12;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -38,7 +42,26 @@ public final class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        startAPICall();
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.cameraType);
+//create a list of items for the spinner.
+        String[] items = new String[]{"Front Hazard Avoidance Camera", "Rear Hazard Avoidance Camera", "Mast Camera",
+                "Chemistry and Camera Complex", "Mars Hand Lens Imager", "Mars Descent Imager",
+                "Navigation Camera", "Panoramic Camera", "Mini-TES"};
+//create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+
+        final Button getImage = findViewById(R.id.get_image);
+        getImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "Start API button clicked");
+                startAPICall();
+            }
+        });
     }
 
     /**
@@ -54,9 +77,17 @@ public final class MainActivity extends AppCompatActivity {
      */
     void startAPICall() {
         try {
+            String roverName = "";
+            String date = "";
+            String camera = "";
+
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                     Request.Method.GET,
-                    "http://api.openweathermap.org/data/2.5/weather?zip=61820,us&appid="
+                    "https://api.nasa.gov/mars-photos/api/v1/rovers/"
+                            + roverName
+                            + "/photos?earth_date="
+                            + date + "&camera="
+                            + camera + "&api_key="
                             + BuildConfig.API_KEY,
                     null,
                     new Response.Listener<JSONObject>() {
